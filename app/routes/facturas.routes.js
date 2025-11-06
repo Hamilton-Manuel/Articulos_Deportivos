b6e4f07fdf163922b21fe4e128b31709b9f15425
Hamilton-Manuel <hmanuelc@miumg.edu.gt>
2025-11-05 20:08:34 -0600

    Corregir ruta del middleware de autenticación y agregar dependencia de pdfkit

--- app/routes/facturas.routes.js
+++ app/routes/facturas.routes.js
@@ -1,12 +1,9 @@
 module.exports = (app) => {
-  const auth = require("../middlewares/auth.js");        // si ya usas auth
+  const auth = require("../middleware/auth.js");        // si ya usas auth
   const fac = require("../controllers/facturas.controller.js");
   const router = require("express").Router();
 
-  // Descargar por session_id de Stripe (lo tienes en PagoExitoso)
   router.get("/session/:session_id.pdf", auth, fac.pdfBySession);
-
-  // (opcional) Descargar por id de pedido
   router.get("/pedido/:id.pdf", auth, fac.pdfByPedido);
 
   app.use("/api/facturas", router);
